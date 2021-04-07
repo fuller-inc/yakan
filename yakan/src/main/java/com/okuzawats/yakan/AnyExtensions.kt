2a0f78f73d05c41c8db6fa08938cd79676856f10
okuzawats <okuzawats@gmail.com>
2021-04-07 12:44:03 +0900

    add isNotNull

--- yakan/src/main/java/com/okuzawats/yakan/AnyExtensions.kt
+++ yakan/src/main/java/com/okuzawats/yakan/AnyExtensions.kt
@@ -15,10 +15,28 @@ import kotlin.contracts.contract
  */
 fun <T : Any?> T?.unwrap(): T = requireNotNull(this)
 
+/**
+ * returns true if the value is null, or returns false if the value is not null.
+ *
+ * @return true if null, else false
+ */
 @OptIn(ExperimentalContracts::class)
 fun <T : Any?> T?.isNull(): Boolean {
     contract {
-        returns() implies (this@isNull != null)
+        returns() implies (this@isNull == null)
     }
     return (this == null)
+}
+
+/**
+ * returns true if the value is not null, or returns false if the value is null.
+ *
+ * @return true if not null, else false
+ */
+@OptIn(ExperimentalContracts::class)
+fun <T : Any?> T?.isNotNull(): Boolean {
+    contract {
+        returns() implies (this@isNotNull != null)
+    }
+    return (this != null)
 }
