008df8d4d1a8263b13bee982ab6d3b53903b149b
okuzawats <okuzawats@gmail.com>
2021-04-08 08:24:08 +0900

    defined pred

--- yakan/src/main/java/com/okuzawats/yakan/IntExtensions.kt
+++ yakan/src/main/java/com/okuzawats/yakan/IntExtensions.kt
@@ -5,5 +5,15 @@ package com.okuzawats.yakan
  *
  * if the value is Int.MAX_VALUE, returns Int.MIN_VALUE
  * @return successor of Int value
+ * @see [pred]
  */
 fun Int.succ(): Int = (this + 1)
+
+/**
+ * returns predecessor of Int value
+ *
+ * if the value is Int.MIN_VALUE, returns Int.MAX_VALUE
+ * @return predecessor of Int value
+ * @see [succ]
+ */
+fun Int.pred(): Int = (this - 1)
