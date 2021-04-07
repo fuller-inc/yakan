42ca37778cd63e3b21f03ea8a7609be58cb44008
okuzawats <okuzawats@gmail.com>
2021-04-08 08:20:55 +0900

    add succ test

--- yakan/src/test/java/com/okuzawats/yakan/IntExtensionsTest.kt
+++ yakan/src/test/java/com/okuzawats/yakan/IntExtensionsTest.kt
@@ -1,5 +1,25 @@
 package com.okuzawats.yakan
 
-import org.junit.Assert.*
+import org.junit.Test
 
-class IntExtensionsTest
+class IntExtensionsTest {
+    @Test
+    fun testSucc_returnsIncrementedValue() {
+        val target = 42
+
+        val actual = target.succ()
+        val expected = 43
+
+        assert(actual == expected)
+    }
+
+    @Test
+    fun testSucc_overflowsIfIntMaxValue() {
+        val target = Int.MAX_VALUE
+
+        val actual = target.succ()
+        val expected = Int.MIN_VALUE
+
+        assert(actual == expected)
+    }
+}
