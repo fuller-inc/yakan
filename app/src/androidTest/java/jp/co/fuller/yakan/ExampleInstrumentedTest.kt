a2da39d36697463ed4ef862119420020dd31cc91
toshiki.okuzawa <toshiki.okuzawa@fuller.co.jp>
2021-07-07 18:29:08 +0900

    update package name

--- app/src/androidTest/java/jp/co/fuller/yakan/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/jp/co/fuller/yakan/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.okuzawats.yakan
+package jp.co.fuller.yakan
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
   fun useAppContext() {
     // Context of the app under test.
     val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-    assertEquals("com.okuzawats.yakan", appContext.packageName)
+    assertEquals("jp.co.fuller.yakan", appContext.packageName)
   }
 }
