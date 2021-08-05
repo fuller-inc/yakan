46cc9363ad78707da49c3de4337bd4c31379889a
toshiki.okuzawa <toshiki.okuzawa@fuller.co.jp>
2021-08-05 14:58:24 +0900

    fixed lint error

--- livedata/src/androidTest/java/jp/co/fuller/yakan/livedata/ExampleInstrumentedTest.kt
+++ livedata/src/androidTest/java/jp/co/fuller/yakan/livedata/ExampleInstrumentedTest.kt
@@ -1,10 +1,10 @@
 package jp.co.fuller.yakan.livedata
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.Assert.assertEquals
-import org.junit.runner.RunWith
 import org.junit.Test
+import org.junit.runner.RunWith
 
 /**
  * Instrumented test, which will execute on an Android device.
