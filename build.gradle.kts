fecae03b97e5a6798f45304a996ff2f3469bef03
toshiki.okuzawa <toshiki.okuzawa@fuller.co.jp>
2021-10-08 15:50:17 +0900

    update project build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,9 +6,9 @@ buildscript {
     dependencies {
         // workaround for version catalogue
         // https://github.com/gradle/gradle/issues/16958
-        // val libs = project.extensions.getByType<VersionCatalogsExtension>().named("libs") as org.gradle.accessors.dm.LibrariesForLibs
+        val libs = project.extensions.getByType<VersionCatalogsExtension>().named("libs") as org.gradle.accessors.dm.LibrariesForLibs
 
-        classpath("com.android.tools.build:gradle:7.0.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+        classpath(libs.android.gradle)
+        classpath(libs.kotlin.gradle)
     }
 }
