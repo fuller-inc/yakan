9214ee2d25dc81671729a856a317b4b45b02b084
toshiki.okuzawa <toshiki.okuzawa@fuller.co.jp>
2021-10-08 15:55:32 +0900

    view/build.gradle to view/build.gradle.kts

---
+++ view/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+}
+
+android {
+    compileSdk = 30
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 30
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                file("proguard-rules.pro")
+            )
+        }
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+kotlin.sourceSets.all {
+    languageSettings.optIn("kotlin.RequiresOptIn")
+}
+
+dependencies {
+    implementation(libs.androidx.core)
+    testImplementation(libs.junit.junit)
+}
