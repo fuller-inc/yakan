017c7ecac633de4177a2a6ea68e0e098b7b60457
toshiki.okuzawa <toshiki.okuzawa@fuller.co.jp>
2022-03-18 11:47:08 +0900

    define maven-publish task: view module

--- view/build.gradle.kts
+++ view/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     kotlin("android")
+    `maven-publish`
 }
 
 android {
@@ -34,4 +35,27 @@ kotlin.sourceSets.all {
 dependencies {
     implementation(libs.androidx.core)
     testImplementation(libs.junit.junit)
+}
+
+tasks {
+    val androidSourcesJar by creating(Jar::class) {
+        archiveClassifier.set("sources")
+        from(android.sourceSets["main"].java.srcDirs)
+    }
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            repositories.maven {
+                url = uri("$rootDir/repository")
+            }
+            create<MavenPublication>("maven") {
+                artifact(tasks["androidSourcesJar"])
+                version = "1.2.0"
+                groupId = "jp.co.fuller"
+                artifactId = "yakan.view"
+            }
+        }
+    }
 }
